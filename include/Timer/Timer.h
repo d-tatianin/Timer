846c8ef6b28acff81d218e0a8e664825ed0d40d7
8infy <99danilt@gmail.com>
2019-08-10 16:24:35 +0300

    Added a new 'AUTO' option, improved timer precision, refactored and cleaned up the code.

--- include/Timer/Timer.h
+++ include/Timer/Timer.h
@@ -3,52 +3,87 @@
 #include <stdio.h>
 #include <chrono>
 
+namespace TimeUnit {
+
+	enum Unit : int
+	{
+		AUTO = 0,
+		NANOSECONDS, 
+		MICROSECONDS, 
+		MILLISECONDS, 
+		SECONDS, 
+		MINUTES
+	};
+
+	const char* UnitToString(Unit unit)
+	{
+		switch (unit)
+		{
+		case NANOSECONDS:
+			return "nanoseconds";
+		case MICROSECONDS:
+			return "microseconds";
+		case MILLISECONDS:
+			return "milliseconds";
+		case SECONDS:
+			return "seconds";
+		case MINUTES:
+			return "minutes";
+		default:
+			return nullptr;
+		}
+	}
+}
+
 class Timer
 {
-public:
-	enum TimeUnit : int
-	{
-		NANOSECONDS = 0, MICROSECONDS, MILLISECONDS, SECONDS, MINUTES
-	};
 private:
-	const char* m_Name;
-	TimeUnit m_Unit;
-	std::chrono::time_point<std::chrono::steady_clock> m_Start;
-	std::chrono::time_point<std::chrono::steady_clock> m_End;
-	std::chrono::duration<double> m_Duration;
-	bool m_ShowResult;
+	typedef std::chrono::time_point<std::chrono::high_resolution_clock>
+		high_precision_tp;
+	typedef std::chrono::duration<double, std::nano>
+		high_precision_dr;
+
+private:
+	high_precision_tp m_Start;
+	high_precision_tp m_End;
+    high_precision_dr m_Duration;
+	double            m_PreciseDuration;
+	TimeUnit::Unit    m_Unit;
+	TimeUnit::Unit    m_AutoUnit;
+	bool              m_ShowResult;
+	const char*       m_Name;
 public:
 	Timer()
-		: m_Name("Unknown"), m_Unit(MILLISECONDS), m_ShowResult(true)
+		: m_Name("Unnamed"), m_Unit(TimeUnit::AUTO), m_ShowResult(true)
 	{
 		m_Start = std::chrono::high_resolution_clock::now();
 	}
 
-	Timer(TimeUnit unit)
-		: m_Name("Unknown"), m_Unit(unit), m_ShowResult(true)
+	Timer(TimeUnit::Unit unit)
+		: m_Name("Unnamed"), m_Unit(unit), m_ShowResult(true)
 	{
 		m_Start = std::chrono::high_resolution_clock::now();
 	}
 
 	Timer(const char* name)
-		: m_Name(name), m_Unit(MILLISECONDS), m_ShowResult(true)
+		: m_Name(name), m_Unit(TimeUnit::AUTO), m_ShowResult(true)
 	{
 		m_Start = std::chrono::high_resolution_clock::now();
 	}
 
 	Timer(const char* name, bool printResultUponDestruction)
-		: m_Name(name), m_Unit(MILLISECONDS), m_ShowResult(printResultUponDestruction)
+		: m_Name(name), m_Unit(TimeUnit::AUTO), m_ShowResult(printResultUponDestruction)
 	{
 		m_Start = std::chrono::high_resolution_clock::now();
 	}
 
-	Timer(const char* name, TimeUnit unit)
+	Timer(const char* name, TimeUnit::Unit unit)
 		: m_Name(name), m_Unit(unit), m_ShowResult(true)
 	{
 		m_Start = std::chrono::high_resolution_clock::now();
 	}
 
-	Timer(const char* name, TimeUnit unit, bool printResultUponDestruction)
+	Timer(const char* name, TimeUnit::Unit unit, bool printResultUponDestruction)
 		: m_Name(name), m_Unit(unit), m_ShowResult(printResultUponDestruction)
 	{
 		m_Start = std::chrono::high_resolution_clock::now();
@@ -62,26 +97,18 @@ public:
 	void ResetAndShowResult()
 	{
 		m_End = std::chrono::high_resolution_clock::now();
-		m_Duration = m_End - m_Start;
+		m_Duration =  m_End - m_Start;
+		m_PreciseDuration = m_Duration.count();
 
-		switch (m_Unit)
-		{
-			case NANOSECONDS:
-				printf("Function \"%s\" took: %f nanoseconds.\n", m_Name, Convert(m_Duration.count()));
-				break;
-			case MICROSECONDS:
-				printf("Function \"%s\" took: %f microseconds.\n", m_Name, Convert(m_Duration.count()));
-				break;
-			case MILLISECONDS:
-				printf("Function \"%s\" took: %f ms.\n", m_Name, Convert(m_Duration.count()));
-				break;
-			case SECONDS:
-				printf("Function \"%s\" took: %f seconds.\n", m_Name, m_Duration.count());
-				break;
-			case MINUTES:
-				printf("Function \"%s\" took: %f minutes.\n", m_Name, Convert(m_Duration.count()));
-				break;
-		}
+		if (m_Unit != TimeUnit::NANOSECONDS)
+			ConvertTime();
+
+		printf("Function \"%s\" took %f %s.\n", 
+			m_Name, 
+			m_PreciseDuration, 
+			m_Unit == TimeUnit::AUTO ? 
+			TimeUnit::UnitToString(m_AutoUnit) : 
+			TimeUnit::UnitToString(m_Unit));
 
 		m_Start = std::chrono::high_resolution_clock::now();
 	}
@@ -90,21 +117,29 @@ public:
 	{
 		m_End = std::chrono::high_resolution_clock::now();
 		m_Duration = m_End - m_Start;
+		m_PreciseDuration = m_Duration.count();
+
+		if (m_Unit != TimeUnit::NANOSECONDS)
+			ConvertTime();
 
 		m_Start = std::chrono::high_resolution_clock::now();
 
-		return Convert(m_Duration.count());
+		return m_PreciseDuration;
 	}
 
 	double GetTime()
 	{
 		m_End = std::chrono::high_resolution_clock::now();
 		m_Duration = m_End - m_Start;
+		m_PreciseDuration = m_Duration.count();
 
-		return Convert(m_Duration.count());
+		if (m_Unit != TimeUnit::NANOSECONDS)
+			ConvertTime();
+
+		return m_PreciseDuration;
 	}
 
-	void SetTimeUnit(TimeUnit unit)
+	void SetTimeUnit(TimeUnit::Unit unit)
 	{
 		m_Unit = unit;
 	}
@@ -115,17 +150,48 @@ public:
 			ResetAndShowResult();
 	}
 private:
-	inline double Convert(double&& seconds)
+	void ConvertTime()
 	{
 		switch (m_Unit)
 		{
-			case NANOSECONDS:  return 1000000000.0 * seconds;
-			case MICROSECONDS: return 1000000.0 * seconds;
-			case MILLISECONDS: return 1000.0 * seconds;
-			case SECONDS:      return seconds;
-			case MINUTES:      return seconds / 60.0;
+			case TimeUnit::AUTO:
+				if (m_PreciseDuration >= 60000000000)
+				{
+					m_PreciseDuration /= 60000000000;
+					m_AutoUnit = TimeUnit::MINUTES;
+				}
+				else if (m_PreciseDuration >= 1000000000)
+				{
+					m_PreciseDuration /= 1000000000;
+					m_AutoUnit = TimeUnit::SECONDS;
+				}
+				else if (m_PreciseDuration >= 1000000)
+				{
+					m_PreciseDuration /= 1000000;
+					m_AutoUnit = TimeUnit::MILLISECONDS;
+				}
+				else if (m_PreciseDuration >= 1000)
+				{
+					m_PreciseDuration /= 1000;
+					m_AutoUnit = TimeUnit::MICROSECONDS;
+				}
+				else
+					m_AutoUnit = TimeUnit::NANOSECONDS;
+				break;
+			case TimeUnit::MICROSECONDS:
+				m_PreciseDuration /= 1000;
+				break;
+			case TimeUnit::MILLISECONDS:
+				m_PreciseDuration /= 1000000;
+				break;
+			case TimeUnit::SECONDS:
+				m_PreciseDuration /= 1000000000;
+				break;
+			case TimeUnit::MINUTES:
+				m_PreciseDuration /= 60000000000;
+				break;
+			default:
+				break;
 		}
-
-		return -1.0;
 	}
 };
